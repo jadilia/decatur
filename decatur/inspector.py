679523fd7e67e5df70bdaddf85efcc0ea8c6ff36
jlurie <luriejcc@gmail.com>
2016-10-05 15:18:02 -0700

    Added ability to manually classfiy light curves.

--- decatur/inspector.py
+++ decatur/inspector.py
@@ -6,16 +6,42 @@ Visually inspect light curves and periodograms
 
 from __future__ import print_function, division, absolute_import
 
+import os
+
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import h5py
 
 from . import config, eclipsing_binary, utils
 
 
+def _create_results_file(merge):
+    """
+    Return the relevant columns of the merged rotation period and EB catalog.
+
+    Parameters
+    ----------
+    merge : pandas DataFrame
+        The merged DataFrame
+
+    Returns
+    -------
+    results : pandas DataFrame
+        Relevant columns for light curve inspection.
+    """
+    results = merge.loc[:, ['KIC', 'period', 'p_rot_1', 'peak_power_1']]
+    results.loc[:, 'class'] = np.repeat('-1', len(results))
+    results.loc[:, 'p_rot_alt'] = np.repeat([-1.], len(results))
+
+    return results
+
+
 class InspectorGadget(object):
     """
     Inspect light curves and periodograms
+
+    Results will be saved in a pickled pandas DataFrame
 
     Parameters
     ----------
@@ -23,17 +49,25 @@ class InspectorGadget(object):
         Name of the pickle file containing the rotation periods.
     periodograms_file : str
         Name of the HDF5 file containing the periodograms.
+    results_file : str
+        Specify an alternate pickle file for the inspection results.
     catalog_file : str, optional
         Specify an alternate eclipsing binary catalog filename.
     """
-    def __init__(self, p_rot_file, periodograms_file, catalog_file='kebc.csv'):
+    def __init__(self, p_rot_file, periodograms_file,
+                 results_file='inspect.pkl', catalog_file='kebc.csv'):
         self.p_rot_file = p_rot_file
         self.catalog_file = catalog_file
 
         merge = utils.merge_catalogs(catalog_file, p_rot_file)
-        self.kics = merge['KIC']
-        self.p_orbs = merge['period']
-        self.p_rots = merge['p_rot_1']
+
+        self.results_file = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                         'data', results_file))
+        if os.path.exists(self.results_file):
+            self.results = pd.read_pickle(self.results_file)
+        else:
+            self.results = _create_results_file(merge)
+            self.results.to_pickle(self.results_file)
 
         # Load the periodograms
         self.h5 = h5py.File('{}/{}'.format(config.data_dir, periodograms_file))
@@ -86,11 +120,15 @@ class InspectorGadget(object):
         index : int
             Index of the system in the results DataFrame.
         """
-        print('\nKIC {}'.format(self.kics[index]))
-        print('P_orb  P_rot')
-        print('----------------')
-        print('{:>5.2f} {:>5.2f}\n'.format(self.p_orbs[index],
-                                           self.p_rots[index]))
+        print('\nKIC {}'.format(self.results['KIC'][index]))
+        print('-----------------------------------')
+        print('P_orb    P_rot     class  p_rot_alt')
+        print('-----------------------------------')
+        header = '{:>6.2f}  {:>5.2f}  {:>10s}  {:>5.2f} \n'
+        print(header.format(self.results['period'][index],
+                            self.results['p_rot_1'][index],
+                            self.results['class'][index],
+                            self.results['p_rot_alt'][index]))
 
     def _update(self, index):
         """
@@ -104,7 +142,9 @@ class InspectorGadget(object):
         if not plt.fignum_exists(self.fig_number):
             self._setup()
 
-        eb = eclipsing_binary.EclipsingBinary.from_kic(self.kics[index],
+        kic = self.results['KIC'][index]
+
+        eb = eclipsing_binary.EclipsingBinary.from_kic(kic,
                                                        catalog_file=self.catalog_file)
         eb.detrend_and_normalize()
 
@@ -117,18 +157,38 @@ class InspectorGadget(object):
         flux_max = np.percentile(np.abs(eb.l_curve.fluxes), 98)
         self.ax1.set_ylim(-flux_max, flux_max)
 
-        periods = self.h5['{}/periods'.format(self.kics[index])][:]
-        powers = self.h5['{}/powers'.format(self.kics[index])][:]
+        periods = self.h5['{}/periods'.format(kic)][:]
+        powers = self.h5['{}/powers'.format(kic)][:]
         self.periodogram.set_xdata(periods)
         self.periodogram.set_ydata(powers)
 
         self.ax2.set_ylim(0, 1.1 * powers.max())
 
-        self.p_rot_line.set_xdata(self.p_rots[index])
+        self.p_rot_line.set_xdata(self.results['p_rot_1'][index])
 
         self.fig.canvas.draw()
 
         self._print_kic_stats(index)
+
+    def _classify(self, index):
+        """
+        User classification of the light curve.
+
+        Parameters
+        ----------
+        index : int
+            Index of the system in the results DataFrame
+        """
+        user_class = raw_input('\nType of out-of-eclipse variability: ').lower()
+        self.results.loc[index, 'class'] = str(user_class)
+
+        alternate_p_rot = raw_input('Alternate rotation period?: ').lower()
+
+        if alternate_p_rot == 'y':
+            user_p_rot = raw_input('Rotation period: ')
+            self.results.loc[index, 'p_rot_alt'] = float(user_p_rot)
+
+        self.results.to_pickle(self.results_file)
 
     def gogo_gadget(self):
         """
@@ -146,7 +206,7 @@ class InspectorGadget(object):
             if user_input == '':
                 continue
             elif user_input == 'n':
-                if ii + 2 > len(self.kics):
+                if ii + 2 > len(self.results['KIC']):
                     print('Reached end of catalog')
                 else:
                     ii += 1
@@ -164,12 +224,16 @@ class InspectorGadget(object):
 
             elif utils.is_int(user_input):
                 try:
-                    index = np.where(int(user_input) == self.kics)[0][0]
+                    index = np.where(int(user_input) == self.results['KIC'])[0][0]
                     ii = index
                 except IndexError:
                     print('Invalid KIC')
                     continue
                 self._update(index)
+
+            elif user_input == 'c':
+                self._classify(ii)
+                self._print_kic_stats(ii)
 
             else:
                 print('Input not understood')
